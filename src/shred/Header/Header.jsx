86f8747e0f26e62aa28f417ade3af6ec4bf61118
dev-abulhassan <abulhassan.dev@gmail.com>
2023-05-21 22:56:50 +0600

    live link

--- src/shred/Header/Header.jsx
+++ src/shred/Header/Header.jsx
@@ -74,7 +74,6 @@ const Header = () => {
             <div className={`md:hidden  ${isOpen ? "block" : "hidden"}`}>
                <div className="px-2 pt-2 pb-3 space-y-1 ">
                   <Link to={'/'} className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">Home</Link >
-                  <Link to={'/'} className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">Recipes</Link >
                   <Link to={'/contact'} className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">Contact</Link >
                   <Link to={'/blog'} className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">Blog</Link >
                </div>
